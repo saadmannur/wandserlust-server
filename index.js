5c255b22bffd8e18033b5231e92a5af2f0e0bc91
saadmannur <saadmannur99@gmail.com>
2026-06-18 16:26:28 +0600

    featured api added

--- index.js
+++ index.js
@@ -114,6 +114,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/featured', async (req, res) => {
+            const result = await destinationCollection.find().limit(4).toArray()
+            res.send(result)
+        })
+
 
 
         // await client.db("admin").command({ ping: 1 });
